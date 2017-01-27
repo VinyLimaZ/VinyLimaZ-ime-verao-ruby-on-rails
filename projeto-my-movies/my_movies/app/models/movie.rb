742c57448d6f3fb316e60ff1885a80245a60e684
VinyLimaZ <vinylimaz@live.com>
2017-01-27 21:48:21 -0200

    associantions, must fix

--- projeto-my-movies/my_movies/app/models/movie.rb
+++ projeto-my-movies/my_movies/app/models/movie.rb
@@ -3,4 +3,7 @@ class Movie < ApplicationRecord
   validates :title, uniqueness:
     {scope: :release_date,
       message: "Should not have two movies with same name and release date"}
+  belongs_to :director, class_name: 'Director', foreign_key: :director_id, optional: true
+  has_and_belongs_to_many :actors, association_foreign_key: :actor_id, join_table: :actor_movies
+
 end
